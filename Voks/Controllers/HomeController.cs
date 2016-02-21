4da4230c25cccf04d1416720eee7002a317a57a0
Stanislav Fajfr <stanislav@fajfr.cz>
2016-02-21 13:01:35 +0100

    Implementuj základní logiku.

--- Voks/Controllers/HomeController.cs
+++ Voks/Controllers/HomeController.cs
@@ -7,21 +7,22 @@ namespace Voks.Controllers
 {
     public class HomeController : Controller
     {
-        private string CategoriesPath => Server.MapPath("~/Content/Images/Categories");
+        private const string CATEGORIES_PATH = "~/Content/Images/Categories";
 
         public ActionResult Index()
         {
-            var viewModel = new MainViewModel();
-            viewModel.Categories = Directory.EnumerateDirectories(CategoriesPath)
-                .Select(dir => new CategoryViewModel()
-                {
-                    Name = Path.GetFileName(dir),
-                    Images = Directory.EnumerateFiles(dir).Select(file => Path.GetFileName(file))
-                })
-                .OrderBy(c => c.Name)
-                .ToArray();
-
-            viewModel.CategoriesBasePath = CategoriesPath;
+            var viewModel = new MainViewModel()
+            {
+                Categories = Directory.EnumerateDirectories(Server.MapPath(CATEGORIES_PATH))
+                    .Select(dir => new CategoryViewModel()
+                    {
+                        Name = Path.GetFileName(dir),
+                        Images = Directory.EnumerateFiles(dir).Select(file => Path.GetFileName(file))
+                    })
+                    .OrderBy(c => c.Name)
+                    .ToArray(),
+                CategoriesBasePath = Url.Content(CATEGORIES_PATH)
+            };
 
             return View(viewModel);
         }
--- Voks/Scripts/app.js
+++ Voks/Scripts/app.js
@@ -1,1 +1,58 @@
-﻿
+﻿$(function () {
+    'use strict';
+
+    var categoriesBasePath = $('#categories-base-path').val();
+    var $availableImages = $('#images > .scrolls');
+    var $selectedImages = $('#selected-images > .scrolls');
+
+    var createImageHtml = function (imagePath) {
+        var fullPath = categoriesBasePath + '/' + imagePath;
+        return '<div class="selectable-box"><img src="' + fullPath + '" /></div>';
+    };
+
+    // categories
+    var selectCategory = function ($category) {
+        $('.category.selected').removeClass('selected');
+        $category.addClass('selected');
+
+        var category = $category.data('category');
+
+        var newAvailableImages = '';
+        category.Images.forEach(function (imageName) {
+            newAvailableImages += createImageHtml(category.Name + '/' + imageName);
+        });
+
+        $availableImages.html(newAvailableImages);
+    };
+
+    $(document).on('click', '.category', function () {
+        selectCategory($(this));
+    });
+    selectCategory($('.category:first'));
+
+    $('.category').each(function () {
+        var $category = $(this);
+        var category = $category.data('category');
+
+        var thumbmailsHtml = '';
+        for (var i = 0; i < 4; i++) {
+            var imagePath = categoriesBasePath + '/' + category.Name + '/' + category.Images[i];
+            thumbmailsHtml += '<img src="' + imagePath + '" />';
+        }
+
+        $category.find('.category-thumbmails').html(thumbmailsHtml);
+    });
+
+    // category images
+    $availableImages.on('click', '.selectable-box', function () {
+        $selectedImages.append(this.outerHTML)
+    });
+
+    // selected images
+    $selectedImages.on('click', '.selectable-box', function () {
+        var $image = $(this);
+        $image.animate({ width: 0 }, 300, 'linear', function () {
+            $image.remove();
+        });
+    });
+});
