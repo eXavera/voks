27d9592abbaf9725e87f30933f353d957e74fe64
Stanislav Fajfr <stanislav@fajfr.cz>
2016-07-30 16:46:45 +0200

    Refactoruj aplikační JS.

--- Voks/Scripts/app.js
+++ Voks/Scripts/app.js
@@ -1,65 +1,88 @@
 ﻿$(function () {
     'use strict';
 
-    var getCategoryThumbmailImages = function (category) {
-        var images = [];
-        for (var i = 0; i < 4 && i < category.Images.length; i++) {
-            images.push(category.Images[i]);
-        }
-        return images;
-    };
-
     var categoriesBasePath = $('#categories-base-path').val();
-    var $availableImages = $('#images > .scrolls');
-    var $selectedImages = $('#selected-images > .scrolls');
 
-    var createImageHtml = function (imagePath) {
-        var fullPath = categoriesBasePath + '/' + imagePath;
-        return '<div class="selectable-box"><img src="' + fullPath + '" /></div>';
-    };
+    var setupCategories = function (callbacks) {
+        var getThumbmailImages = function (category) {
+            var images = [];
+            for (var i = 0; i < 4 && i < category.Images.length; i++) {
+                images.push(category.Images[i]);
+            }
+            return images;
+        };
 
-    // categories
-    var selectCategory = function ($category) {
-        $('.category.selected').removeClass('selected');
-        $category.addClass('selected');
+        var selectCategory = function ($category) {
+            $('.category.selected').removeClass('selected');
+            $category.addClass('selected');
 
-        var category = $category.data('category');
+            callbacks.selected($category.data('category'));
+        };
 
-        var newAvailableImages = '';
-        category.Images.forEach(function (imageName) {
-            newAvailableImages += createImageHtml(category.Name + '/' + imageName);
+        $('.category').each(function () {
+            var $category = $(this);
+            var category = $category.data('category');
+
+            var thumbmailImages = getThumbmailImages(category);
+
+            var thumbmailsHtml = '';
+            for (var i = 0; i < thumbmailImages.length; i++) {
+                var imagePath = categoriesBasePath + '/' + category.Name + '/' + thumbmailImages[i];
+                thumbmailsHtml += '<img src="' + imagePath + '" />';
+            }
+
+            $category.find('.category-thumbmails').html(thumbmailsHtml);
         });
 
-        $availableImages.html(newAvailableImages);
+        $(document).on('click', '.category', function () {
+            selectCategory($(this));
+        });
+        selectCategory($('.category:first'));
     };
 
-    $(document).on('click', '.category', function () {
-        selectCategory($(this));
+    var setupAvailableImages = function (callbacks) {
+        var $availableImages = $('#images > .scrolls');
+        var createImageHtml = function (imagePath) {
+            var fullPath = categoriesBasePath + '/' + imagePath;
+            return '<div class="selectable-box"><img src="' + fullPath + '" /></div>';
+        };
+        var refresh = function (category) {
+            var newAvailableImages = '';
+            category.Images.forEach(function (imageName) {
+                newAvailableImages += createImageHtml(category.Name + '/' + imageName);
+            });
+
+            $availableImages.html(newAvailableImages);
+        };
+
+        $availableImages.on('click', '.selectable-box', function () {
+            callbacks.selected(this);
+        });
+
+        return {
+            refresh: refresh
+        };
+    };
+
+    var selectedImages = (function () {
+        var $selectedImages = $('#selected-images > .scrolls');
+        var appendImage = function (image) {
+            $selectedImages.append(image.outerHTML);
+        };
+
+        $selectedImages.on('click', '.selectable-box', function () {
+            $(this).remove();
+        });
+
+        return {
+            appendImage: appendImage
+        };
+    })();
+
+    var availableImages = setupAvailableImages({
+        selected: selectedImages.appendImage
     });
-    selectCategory($('.category:first'));
-
-    $('.category').each(function () {
-        var $category = $(this);
-        var category = $category.data('category');
-
-        var thumbmailImages = getCategoryThumbmailImages(category);
-
-        var thumbmailsHtml = '';
-        for (var i = 0; i < thumbmailImages.length; i++) {
-            var imagePath = categoriesBasePath + '/' + category.Name + '/' + thumbmailImages[i];
-            thumbmailsHtml += '<img src="' + imagePath + '" />';
-        }
-
-        $category.find('.category-thumbmails').html(thumbmailsHtml);
-    });
-
-    // category images
-    $availableImages.on('click', '.selectable-box', function () {
-        $selectedImages.append(this.outerHTML)
-    });
-
-    // selected images
-    $selectedImages.on('click', '.selectable-box', function () {
-        $(this).remove();
+    setupCategories({
+        selected: availableImages.refresh
     });
 });
